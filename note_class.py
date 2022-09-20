e2bf979953197d30d52e3e4694b0c9bc3ce30f61
paigni <ikontai4@gmail.com>
2022-09-20 20:26:16 +0600

    Изменена логика работы программы, переделаны функции

--- note_class.py
+++ note_class.py
@@ -16,26 +16,28 @@ class Note:
     def read_note(self):
         with open('notes.json', 'r') as write_file:
             data = json.load(write_file)
-        for note in data:
-            print(note)
+            return data
 
     def save_changes(self, data):
         with open('notes.json', 'w') as write_file:
             json.dump(data, write_file)
 
+    def show_notes(self):
+        count = 0
+        for note in self.read_note():
+            count += 1
+            print(f"{count}){note['header']} {note['text']} {note['time_create']} {note['time_update']}")
 
-    def action_over_note(self, header, text):
-        with open('notes.json', 'r') as write_file:
-            data = json.load(write_file)
-
+    def create_new_note(self, header, text):
+        check_file = os.path.exists('notes.json')
+        if not check_file:
+            data = []
+            self.save_changes(data)
+        data = self.read_note()
         self.__text = text
         self.__header = header
-        if self.__time_create == '':
-            self.__time_create = self.now()
-            self.__time_update = ''
-        else:
-            self.__time_update = self.now()
-
+        self.__time_create = self.now()
+        self.__time_update = ''
         note = {
             'header': self.__header,
             'text': self.__text,
@@ -45,12 +47,21 @@ class Note:
         data.append(note)
         self.save_changes(data)
 
-    def create_new_file(self,header,text):
-        check_file = os.path.exists('notes.json')
-        if not check_file:
-            data = []
-            self.save_changes(data)
-            self.action_over_note(header, text)
-        else:
-            self.action_over_note(header,text)
+    def change_note(self, header, text, count):
+        data = self.read_note()[count]
+        self.__text = text
+        self.__header = header
+        self.__time_create = self.__time_create
+        self.__time_update = self.now()
+        note = {
+            'header': self.__header,
+            'text': self.__text,
+            'time_create': self.now(),
+            'time_update': self.__time_update
+        }
+        data.append(note)
+        self.save_changes(data)
 
+    def delete_note(self, user_inp):
+        data = self.read_note()
+        data.pop(user_inp)
