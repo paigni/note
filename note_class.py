bf0ad889da4de51e2fa07ae76c36edb34b2328c6
paigni <ikontai4@gmail.com>
2022-09-22 20:26:35 +0600

    Изменена логика работы программы, переделаны функции

--- note_class.py
+++ note_class.py
@@ -14,49 +14,48 @@ class Note:
         return now.strftime("%d-%m-%Y %H:%M")
 
     def read_note(self):
-        with open('notes.json', 'r') as write_file:
-            data = json.load(write_file)
-            return data
+        check_file = os.path.exists('notes.json')
+        if not check_file:
+            data = []
+            self.save_changes(data)
+        else:
+            with open('notes.json', 'r') as write_file:
+                data = json.load(write_file)
+                return data
 
     def save_changes(self, data):
         with open('notes.json', 'w') as write_file:
             json.dump(data, write_file)
 
     def show_notes(self):
-        count = 0
-        for note in self.read_note():
-            count += 1
-            print(f"{count}){note['header']} {note['text']} {note['time_create']} {note['time_update']}")
-
-    def create_new_note(self, header, text):
         check_file = os.path.exists('notes.json')
         if not check_file:
             data = []
             self.save_changes(data)
-        data = self.read_note()
-        self.__text = text
-        self.__header = header
-        self.__time_create = self.now()
-        self.__time_update = ''
-        note = {
-            'header': self.__header,
-            'text': self.__text,
-            'time_create': self.now(),
-            'time_update': self.__time_update
-        }
-        data.append(note)
-        self.save_changes(data)
+        count = 0
+        notes = self.read_note()
+        for note in notes:
+            count += 1
+            print(f"{count}){note['header']} {note['text']} {note['time_create']} {note['time_update']}")
 
-    def change_note(self, header, text, count):
-        data = self.read_note()[count]
+    def action_with_note(self, header, text, count=99999):
+        check_file = os.path.exists('notes.json')
+        if not check_file:
+            data = []
+            self.save_changes(data)
         self.__text = text
         self.__header = header
-        self.__time_create = self.__time_create
+        if count == 99999:
+            self.__time_create = self.now()
+            data = self.read_note()
+        elif count < 99999:
+            data = self.read_note()[count]
+            self.__time_create = self.__time_create
         self.__time_update = self.now()
         note = {
             'header': self.__header,
             'text': self.__text,
-            'time_create': self.now(),
+            'time_create': self.__time_create,
             'time_update': self.__time_update
         }
         data.append(note)
@@ -64,4 +63,4 @@ class Note:
 
     def delete_note(self, user_inp):
         data = self.read_note()
-        data.pop(user_inp)
+        data.pop(user_inp-1)
